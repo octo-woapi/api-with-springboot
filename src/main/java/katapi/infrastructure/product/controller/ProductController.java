5a654c25307577511ed0d66b428aeed1a2e9c2b0
Rafaelle <rbotter@octo.com>
2018-05-25 17:16:47 +0200

    import du bon package dans le controller

--- src/main/java/katapi/infrastructure/product/controller/ProductController.java
+++ src/main/java/katapi/infrastructure/product/controller/ProductController.java
@@ -9,13 +9,13 @@ import org.h2.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sun.jvm.hotspot.debugger.Page;
 
 import javax.validation.constraints.NotNull;
 import java.awt.print.Pageable;
